a28aebc2cc737f2a1cf9f063f413227d99f28276
diosaurusss <122851490+diosaurusss@users.noreply.github.com>
2023-01-22 01:05:58 +0700

    Update jenis.py

--- animal/pages/jenis.py
+++ animal/pages/jenis.py
@@ -8,12 +8,12 @@ with tab1:
    st.markdown('1.   SINGA')
    st.image("animal/pages/singa.jpg")
    st.markdown('berikut ini adalah suara singa')
-   st.audio("animal/pages/suara singa.mp3")
+   st.audio("animal/pages/suara singa.ogg")
    
    st.markdown('1.   HARIMAU')
    st.image("animal/pages/harimau.jpg")
    st.markdown('berikut ini adalah suara harimau')
-   st.audio("animal/pages/suara singa.mp3")
+   st.audio("animal/pages/suara harimau.mp3")
 
 with tab2:
    st.header("HERBIVORA")
