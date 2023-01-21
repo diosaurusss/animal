934e442a8083e643afd7b909048b55b1189b1ec5
diosaurusss <122851490+diosaurusss@users.noreply.github.com>
2023-01-22 01:15:14 +0700

    Update Home.py

--- animal/Home.py
+++ animal/Home.py
@@ -1,7 +1,8 @@
 
 import streamlit as st
-st.sidebar.success("PILIH HALAMAN")
+
 st.set_page_config(
     page_title="ANIMALCLUB")
 
 st.title("SELAMAT DATANG DI ANIMAL CLUB")
+st.sidebar.success("PILIH HALAMAN")
