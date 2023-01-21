b8fa67978996702ef9b3af0cec40576fa785fb04
diosaurusss <122851490+diosaurusss@users.noreply.github.com>
2023-01-22 02:11:02 +0700

    Update Home.py

--- animal/Home.py
+++ animal/Home.py
@@ -10,26 +10,34 @@ st.sidebar.success("Pilih Halaman")
 st.subheader("Hai Sobat")
     
 st.write(
-        "Selamat Datang Di WEB Pengenalan Hewan"
+        "Selamat Datang Di WEB Pengenalan Hewan Vertebrata"
     )
 
 st.write("---")
 
-st.header("Apa Itu Hewan?")
+st.header("Apa Itu Hewan Vertebrata?")
 st.write("##")
-st.write("Pengertian Hewan")
+st.write("Pengertian Hewan Vertebrata")
 st.write(
             """
-                Menurut Anshori (2009) hewan atau disebut juga dengan binatang adalah 
-            kelompok organisme yang diklasifikasikan dalam kerajaan Animalia atau 
-            metazoa, adalah salah satu dari berbagai makhluk hidup di bumi. Sebutan lainnya 
-            adalah fauna dan margasatwa (atau satwa saja). Hewan dalam pengertian 
-            sistematika modern mencakup hanya kelompok bersel banyak (multiselular) dan 
-            terorganisasi dalam fungsi-fungsi yang berbeda (jaringan), sehingga kelompok ini 
-            disebut juga histozoa. Semua binatang heterotrof, artinya tidak membuat energi 
-            sendiri, tetapi harus mengambil dari lingkungan sekitarnya.
+            Hewan vertebrata adalah golongan hewan yang memiliki tulang belakang.
+            Tulang belakang berasal dari perkembangan sumbu penyokong tubuh primer atau notokorda (korda dorsalis).
+            Notokorda vertebrata hanya ada pada masa embrionik,
+            setelah dewasa akan mengalami penulangan menjadi sistem penyokong tubuh sekunder, 
+            yaitu tulang belakang (vertebrae).
+
+            Dalam sistem klasifikasi, vertebrata merupakan subfilum dari filum Chordata. 
+            Chordata meliputi hewan-hewan yang memiliki ciri-ciri sebagai berikut :
+
+            1.  Memiliki notokord, yaitu kerangka berbentuk batangan keras tetapi lentur.
+                Notokord terletak di antara saluran pencernaan dan tali saraf, memanjang sepanjang tubuh membentuk sumbu kerangka.
+            2.  Memiliki tali saraf tunggal, berlubang terletak dorsal terhadap notokord, dan memiliki ujung anterior  yang membesar berupa otak.
+            3.  Memiliki ekor yang memanjang ke arah posterior terhadap anus.
+            4.  Memiliki celah faring.
             """
         )
+
+
 st.header("Playlist Fundamental Streamlit")
 col1, col2, col3 = st.columns(3)
 
