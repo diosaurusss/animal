bcff70857e4573bb49a6514862679bbd5de456f9
diosaurusss <122851490+diosaurusss@users.noreply.github.com>
2023-01-22 00:09:57 +0700

    Update jenis.py

--- animal/pages/jenis.py
+++ animal/pages/jenis.py
@@ -4,19 +4,9 @@ tab1, tab2, tab3 = st.tabs(["KARNIVORA", "HERBIVORA", "OMNIVORA"])
 
 with tab1:
    st.header("KARNIVORA")
-   st.caption('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN KARNIVORA : ')
-   col1, col2, col3 = st.columns(3)
-with col1:
-   st.header("SINGA")
+   st.subheader('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN KARNIVORA : ')
+   st.caption("SINGA")
    st.image("singa.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
 
 with tab2:
    st.header("HERBIVORA")
