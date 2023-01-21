fa6ea452d26edb2bacf6e2832e19132b8a2f0dc4
diosaurusss <122851490+diosaurusss@users.noreply.github.com>
2023-01-21 23:54:21 +0700

    Update jenis.py

--- animal/pages/jenis.py
+++ animal/pages/jenis.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["KARNIVORA", "OMNIVORA", "HERBIVORA"])
+tab1, tab2, tab3 = st.tabs(["KARNIVORA", "HERBIVORA", "OMNIVORA"])
 
 with tab1:
    st.header("KARNIVORA")
-   st.caption('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN KARNIVORA')
+   st.caption('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN KARNIVORA : ')
 
 with tab2:
-   st.header("OMNIVORA")
-   st.caption('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN KARNIVORA')
+   st.header("HERBIVORA")
+   st.caption('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN HERBIVORA : ')
 
 with tab3:
-   st.header("HERBIVORA")
-   st.caption('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN KARNIVORA')
+   st.header("OMNIVORA")
+   st.caption('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN OMNIVORA  : ')
