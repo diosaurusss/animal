fee223e65de622e93942349319edeec1da160db7
diosaurusss <122851490+diosaurusss@users.noreply.github.com>
2023-01-22 00:12:57 +0700

    Update jenis.py

--- animal/pages/jenis.py
+++ animal/pages/jenis.py
@@ -5,7 +5,7 @@ tab1, tab2, tab3 = st.tabs(["KARNIVORA", "HERBIVORA", "OMNIVORA"])
 with tab1:
    st.header("KARNIVORA")
    st.subheader('BERIKUT INI ADALAH BEBERAPA CONTOH HEWAN KARNIVORA : ')
-   st.caption("SINGA")
+   st.markdown('1.   SINGA')
    st.image("animal/pages/singa.jpg")
 
 with tab2:
